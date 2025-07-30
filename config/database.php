fa68eedb3eb1b2ee353c5de6ea3c2f0f97c36123
IamNguyenPhuong <163823328+IamNguyenPhuong@users.noreply.github.com>
2025-07-30 20:47:27 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -7,12 +7,7 @@ $username = getenv('DB_USER') ?: 'root';
 $password = getenv('DB_PASS') ?: '';
 
 // Production settings
-$options = [
-    // PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    // PDO::ATTR_EMULATE_PREPARES => false,
-    // PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci"
-];
+$options = [];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, $options);
